1aab0e5928c455312f2582c15d9a2289689429f5
Daniel Micay <danielmicay@gmail.com>
2018-04-18 19:35:56 -0400

    add more non-sensitive system properties

--- filter_prop.sh
+++ filter_prop.sh
@@ -2,6 +2,11 @@
 
 for d in */; do
     if [[ -f "$d/captured.prop" ]]; then
-        grep -e '^\[ro.build' -e '^\[ro.product' "$d/captured.prop" > "$d/filtered.prop"
+        grep '^\[ro\.' "$d/captured.prop" | grep -v \
+            -e '^\[ro\.ap_serial' \
+            -e '^\[ro\.boot\.' \
+            -e '^\[ro\.bootmode' \
+            -e '^\[ro\.em\.did' \
+            > "$d/filtered.prop"
     fi
 done
