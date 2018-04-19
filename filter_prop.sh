007b8e553d96d0a229fb3b2de177df17c188b496
Daniel Micay <danielmicay@gmail.com>
2018-04-18 20:57:15 -0400

    whitelist ro.boot.vbmeta properties

--- filter_prop.sh
+++ filter_prop.sh
@@ -2,11 +2,25 @@
 
 for d in */; do
     if [[ -f "$d/captured.prop" ]]; then
-        grep '^\[ro\.' "$d/captured.prop" | grep -v \
-            -e '^\[ro\.ap_serial' \
-            -e '^\[ro\.boot\.' \
-            -e '^\[ro\.bootmode' \
-            -e '^\[ro\.em\.did' \
-            > "$d/filtered.prop"
+        echo -n > "$d/filtered.prop"
+        cat "$d/captured.prop" | while read line; do
+            # ignore non-read-only properties (effectively read-only ones may be whitelisted later)
+            if [[ "$line" != "[ro."* ]]; then
+                continue
+            fi
+            # ignore ro.bootmode since it varies
+            if [[ "$line" == "[ro.bootmode]"* ]]; then
+                continue
+            fi
+            # ignore personally identifying information inappropriately leaked by Samsung devices
+            if [[ "$line" = "[ro.ap_serial]"* || "$line" = "[ro.em.did]"* ]]; then
+                continue
+            fi
+            # ignore ro.boot.* other than whitelisted exceptions not varying based on boot time, etc.
+            if [[ "$line" = "[ro.boot."* && "$line" != "[ro.boot.vbmeta."* ]]; then
+                continue
+            fi
+            echo "$line" >> "$d/filtered.prop"
+        done
     fi
 done
